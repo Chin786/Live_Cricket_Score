fe3aef61c545cdc2c43a67cab7ecf1e0d994074f
chin786 <chinmaychouhan955@gmail.com>
2021-08-11 10:02:36 +0530

    my first commet reactwebsite

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState} from 'react';
+import './App.css';
+import {Grid} from "@material-ui/core";
+import Navbar from "./Components/Navbar";
+import { getMatches } from "./Api/Api";
+import MyCard from "./Components/MyCard";
+
+
+function App() {
+
+    const [matches,setMatches] = useState([]);
+
+    useEffect(() => {
+        getMatches()
+            .then((data) => setMatches(data.matches))
+            .catch();
+    },[]);
+
+
+  return (
+    <div className="App">
+          <Navbar />
+        <h1><b>Welcome to my Live Status Platform</b></h1>
+           <Grid container>
+             <Grid sm={2}></Grid>
+             <Grid sm={8}>
+                 {
+                     matches.map((match) => (
+                         <MyCard match={match} />))
+                 }
+             </Grid>
+         </Grid>
+    </div>
+  );
+}
+
+export default App;
